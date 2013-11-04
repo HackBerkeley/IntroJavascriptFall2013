daac6d9d1c311a7407d62913f4820ac7298a9c44
Dylan Chun <d.chunwong@gmail.com>
2013-11-04 10:28:40 -0800

    Added skeleton code; removed extra font declaration

---
+++ index_complete.js
@@ -0,0 +1,31 @@
+var pokemon_dict;
+
+if (!pokemon_dict) {
+}
+
+var pokemon_background;
+
+function initializeColor() {
+}
+
+function newBackground() {
+}
+
+function update() {
+}
+
+function getRandomPokemon() {
+}
+
+function isShiny(){
+}
+
+function howManyLeft(){
+}
+
+function reset(){
+
+}
+
+update();
+initializeColor();
